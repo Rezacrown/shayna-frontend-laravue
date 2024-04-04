78357ab16a18da1167ea0f86ad64d74bbb571a85
rizky reza <aryhidayatfebriana@gmail.com>
2024-04-04 22:40:03 +0800

    finish slicing owl caraoseul

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="hero-section">
+    <Carousel :items-to-show="1">
+      <Slide v-for="slide in 3" :key="slide">
+        <div
+          class="single-hero-items set-bg"
+          style="width: 100%; height: 400px"
+          :style="`background-image: url('http://localhost:3000/src/assets/img/hero-${slide}.jpg');`"
+        >
+          <div class="container" style="margin-top: 70px">
+            <div class="row">
+              <div class="col-lg-5">
+                <span>Bag,kids</span>
+                <h1>Black friday</h1>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                  incididunt ut labore et dolore
+                </p>
+                <a href="#" class="primary-btn">Shop Now</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Slide>
+
+      <template #addons>
+        <navigation />
+        <pagination />
+      </template>
+    </Carousel>
+  </section>
+</template>
+
+<script setup lang="ts">
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Navigation } from 'vue3-carousel'
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import HeaderSection from '../components/HeaderSection.vue'
+import HeroSection from '../components/HeroSection.vue'
+</script>
 
 <template>
-  <main>
-    <h3>test</h3>
+  <main style="height: 200vh; overflow-x: hidden">
+    <HeaderSection />
+    <HeroSection />
   </main>
 </template>
